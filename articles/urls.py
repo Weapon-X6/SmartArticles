d8bec8ff7e4125ab57647c1afb7b42d9b7c55c9e
waffe_x20 <argueta_eduardo@yahoo.de>
2022-04-03 16:05:16 -0600

    Adds join and article views!

--- articles/urls.py
+++ articles/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('', views.home, name='home'),
+    path('articles/<int:pk>', views.article, name='article'),
+    path('join', views.join, name='join'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Article
 
 
 def home(request):
     articles = Article.objects
     return render(request, 'articles/home.html', {'articles': articles})
+
+
+def article(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if article.is_premium:
+        return redirect('join')
+    else:
+        return render(request, 'articles/article.html', {'article': article})
+
+
+def join(request):
+    return render(request, 'articles/join.html')
 
