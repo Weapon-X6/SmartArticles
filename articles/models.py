8e6e3ad056d0a553d9406f619eb9c62610f9e669
waffe_x20 <argueta_eduardo@yahoo.de>
2022-04-19 22:50:04 -0600

    Add StripeCostumer model

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=33)
     text = models.TextField()
     is_premium = models.BooleanField(default=True)
 
+
+class StripeCostumer(models.Model):
+    user = models.OneToOneField(to=User,on_delete=models.CASCADE)
+    stripeCustomerId = models.CharField(max_length=255)
+    stripeSubscriptionId = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
